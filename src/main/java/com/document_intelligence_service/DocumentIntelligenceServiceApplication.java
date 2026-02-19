1c205f9824b969637895812f67e81e8d2da22dff
Saksham kumar <agrawalsaksham565@gmail.com>
2026-02-19 20:34:47 +0530

    first commit

---
+++ src/main/java/com/document_intelligence_service/DocumentIntelligenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.document_intelligence_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentIntelligenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentIntelligenceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/document_intelligence_service/DocumentIntelligenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.document_intelligence_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentIntelligenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
